5375962f9455513a4ef5be68f105a8f84761741d
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-23 13:57:05 -0700

    Separated out controllers, preparing to build on app.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
@@ -13,11 +13,11 @@ app.set('view engine', 'pug')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).render('404', pageTitle: 'Page Not Found')
+  res.status(404).render('404', {pageTitle: 'Page Not Found'})
 })
 
 app.listen(3000)
