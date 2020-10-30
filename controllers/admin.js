4ee490dfae1bdacb4d5ff7ded387519f40b61579
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-30 14:01:04 -0700

    Finished edit product routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,7 +12,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl
   const price = req.body.price
   const description = req.body.description
-  const product = new Product(title, imageUrl, description, price)
+  const product = new Product(null, title, imageUrl, description, price)
   product.save()
   res.redirect('/')
 }
@@ -34,6 +34,23 @@ exports.getEditProduct = (req, res, next) => {
       product: product
     })
   })
+}
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  const updatedTitle = req.body.title
+  const updatedPrice = req.body.price
+  const updatedDescription = req.body.description
+  const updatedImageUrl = req.body.imageUrl
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDescription,
+    updatedPrice
+  )
+  updatedProduct.save()
+  res.redirect('/admin')
 }
 
 exports.getProducts = (req, res, next) => {
