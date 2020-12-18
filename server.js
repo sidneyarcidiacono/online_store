520b2c9ddc5a735b0edb13577148d6b82d02ed36
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-18 10:06:00 -0800

    Adding to cart with database.

--- server.js
+++ server.js
@@ -17,19 +17,18 @@ app.set('view engine', 'pug')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) => {
+  User.findById("5fdce27149dc2cdfd2d09bef")
+  .then(user => {
+    req.user = new User(user.name, user.email, user.cart, user._id)
+    next()
+  })
+  .catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-  User.findById("5fdce27149dc2cdfd2d09bef")
-  .then(user => {
-    req.user = user
-    next()
-  })
-  .catch(err => console.log(err))
-  next()
-})
 
 app.use(errorController.show404)
 
