7a12e61ba5f41c34501ca8718ea7d609722a794f
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-20 18:00:08 -0800

    Beginning refactoring project to use mongoose.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const errorController = require('./controllers/errors')
-const mongoConnect = require('./util/database').mongoConnect
-const User = require('./models/user')
+// const User = require('./models/user')
 
 const app = express()
 
@@ -17,14 +18,14 @@ app.set('view engine', 'pug')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use((req, res, next) => {
-  User.findById("5fdce27149dc2cdfd2d09bef")
-  .then(user => {
-    req.user = new User(user.name, user.email, user.cart, user._id)
-    next()
-  })
-  .catch(err => console.log(err))
-})
+// app.use((req, res, next) => {
+//   User.findById("5fdce27149dc2cdfd2d09bef")
+//   .then(user => {
+//     req.user = new User(user.name, user.email, user.cart, user._id)
+//     next()
+//   })
+//   .catch(err => console.log(err))
+// })
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
@@ -32,6 +33,11 @@ app.use(shopRoutes)
 
 app.use(errorController.show404)
 
-mongoConnect(() => {
-  app.listen(3000)
-})
+mongoose.connect(process.env.MONGODB_URI)
+  .then(result => {
+    console.log('CONNECTED')
+    app.listen(3000)
+  })
+  .catch(err => {
+    console.log(err)
+  })
