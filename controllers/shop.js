a77e5fe0e53a997299af044308f2db1b41998806
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-21 09:21:56 -0800

    Refactored adding orders to work with mongoose setup

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product')
+const Order = require('../models/order')
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -81,13 +82,26 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart
   req.user
-    .addOrder()
-    .then(result => {
-      res.redirect('/orders')
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return {quantity: i.quantity, product: { ...i.productId._doc } }
+      })
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      })
+      return order.save()
     })
-    .catch(err => console.log(err))
+      .then(result => {
+        res.redirect('/orders')
+      })
+      .catch(err => console.log(err))
 }
 
 exports.getOrders = (req, res, next) => {
