df2b05b41b84ecbbb402ae7ff7138cafb8f4fc94
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-20 18:20:09 -0800

    Finished refactoring products, admin CRUD routes to use mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -57,25 +57,24 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price
   const updatedImageUrl = req.body.imageUrl
   const updatedDesc = req.body.description
-
-    const product = new Product(
-      updatedTitle,
-      updatedPrice,
-      updatedDesc,
-      updatedImageUrl,
-      prodId
-    )
-    product
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle
+      product.price = updatedPrice
+      product.description = updatedDesc
+      product.imageUrl = updatedImageUrl
+      return product
       .save()
-      .then(result => {
-        console.log('UPDATED PRODUCT!')
-        res.redirect('/admin/products')
-      })
-      .catch(err => console.log(err))
+    })
+    .then(result => {
+      console.log('UPDATED PRODUCT!')
+      res.redirect('/admin/products')
+    })
+    .catch(err => console.log(err))
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -88,7 +87,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       res.redirect('/admin/products')
     })
