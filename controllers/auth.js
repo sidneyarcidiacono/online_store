7bd0c71bb10e88abeb8d966941d9561750d3411b
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-01-02 17:05:02 -0800

    Finished validation for sign up and log in routes.

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,12 +43,22 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0]
+    })
+  }
+
   User.findOne({ email: email })
     .then(user => {
-      if (!user) {
-        req.flash('error', "Invalid email or password")
-        return res.redirect('/login')
-      }
+        if (!user) {
+          req.flash('error', 'Invalid email or password.')
+          return res.redirect('/login')
+        }
       bcrypt.compare(password, user.password)
         .then(doMatch => {
           if (doMatch) {
@@ -74,7 +84,6 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
-  const confirmPassword = req.body.confirmPassword
   const errors = validationResult(req)
   console.log(errors)
   if (!errors.isEmpty()) {
@@ -86,13 +95,7 @@ exports.postSignup = (req, res, next) => {
     })
   }
   // Alternatively, add "unique" to mongo schema to avoid another query/duplicate users with the same email
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', "A user with this email already exists. Please try another.")
-        return res.redirect('/signup')
-      }
-      return bcrypt
+      bcrypt
         .hash(password, 12)
         .then(hashedPassword => {
           const user = new User({
@@ -121,7 +124,6 @@ exports.postSignup = (req, res, next) => {
             })
       })
       .catch(err => console.log(err))
-    })
     .catch(err => {
       console.log(err)
     })
--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,23 @@ const express = require('express')
 const { body } = require('express-validator')
 
 const authController = require('../controllers/auth')
+const User = require('../models/user')
 
 const router = express.Router()
 
 router.get('/login', authController.getLogin)
 
-router.post('/login', authController.postLogin)
+router.post('/login',
+          [
+            body('email')
+            .isEmail()
+            .withMessage('Please enter a valid email.'),
+            body('password', 'Invalid password')
+            .isLength({ min: 5, max: 15 })
+            .isAlphanumeric()
+          ],
+          authController.postLogin
+        )
 
 router.get('/signup', authController.getSignup)
 
@@ -17,15 +28,24 @@ router.post('/signup',
             .isEmail()
             .withMessage('Please enter a valid email.')
             .custom((value, {req}) => {
-              // This is a super random, arbitrary dummy example to demonstrate using the custom method.
-              if (value === 'test@test.com') {
-                throw new Error('This email address is forbidden.')
-              }
-              return true
+              // This will store our promise rejection as an error, and return a Boolean;
+              return User.findOne({ email: value })
+                .then(userDoc => {
+                  if (userDoc) {
+                    return Promise.reject('Email exists already, please pick a different one.')
+                }
+              })
             }),
             body('password', 'Please make sure your password is 5-15 characters and contains no special characters.')
             .isLength({min: 5, max: 15})
-            .isAlphanumeric()
+            .isAlphanumeric(),
+            body('confirmPassword')
+            .custom((value, { req }) => {
+              if (value !== req.body.password) {
+                throw new Error('Passwords have to match.')
+              }
+              return true
+            })
           ],
             authController.postSignup
           )
